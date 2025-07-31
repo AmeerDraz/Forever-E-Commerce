e099b8a256ca47367fc7f50fd023305b744f0aa9
AmeerDraz <ameerabudraz1@gmail.com>
2025-07-31 14:45:09 +0300

    feat: related product copm and add to product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
     const { productId } = useParams();
@@ -133,7 +134,7 @@ const Product = () => {
 
             {/*-------- display related products ------------*/}
 
-            
+            <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
 
         </div>
     ) : (
