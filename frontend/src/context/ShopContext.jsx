76338268bae8853e9ab7b6b1cad5c4282dc63bd0
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-03 15:18:49 +0300

    feat(cart): add CartTotal section to display subtotal, delivery, and total cost

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -41,7 +41,10 @@ const ShopContextProvider = (props) => {
                     if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
                     }
-                } catch (error) {}
+                } catch (error) {
+                    toast.error("Error calculating cart count");
+                    console.error("Error calculating cart count:", error);
+                }
             }
         }
         return totalCount;
@@ -51,6 +54,24 @@ const ShopContextProvider = (props) => {
         let cartDara = structuredClone(cartItems);
         cartDara[itemId][size] = quality;
         setCartItems(cartDara);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+                    toast.error("Error calculating cart amount");
+                    console.error("Error calculating cart amount:", error);
+                }
+            }
+        }
+        return totalAmount;
     }
 
     const value = {
@@ -65,6 +86,7 @@ const ShopContextProvider = (props) => {
         addToCart,
         getCartCount,
         updateQuantity,
+        getCartAmount,
     };
 
     return (
