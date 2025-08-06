a6f2156b8b3e6a516dc4df0564e8511a0f13183b
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-06 14:52:12 +0300

    feat: implement user registration route and controller logic

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 
 // App Config
@@ -18,6 +19,10 @@ app.use(cors())
 
 
 // api endpoints
+app.use('/api/user', userRouter)
+
+
+
 
 app.get('/', (req, res) => {
     res.send('API Working')
