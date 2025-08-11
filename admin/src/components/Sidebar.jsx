d50f70877ec77453979d47d2a0cf245bd9ef5590
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-11 15:01:55 +0300

    feat: add Routes , font , navigate

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -15,14 +15,14 @@ const Sidebar = () => {
                 </NavLink>
                 <NavLink
                     className="flex item-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l"
-                    to="/add"
+                    to="/list"
                 >
                     <img className="w-5 h-5" src={assets.order_icon} alt="" />
                     <p className="hidden md:block">List Items</p>
                 </NavLink>
                 <NavLink
                     className="flex item-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l"
-                    to="/add"
+                    to="/orders"
                 >
                     <img className="w-5 h-5" src={assets.order_icon} alt="" />
                     <p className="hidden md:block">Orders</p>
