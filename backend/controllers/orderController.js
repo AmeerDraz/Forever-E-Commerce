e4cbe35959d433bcaba8acbfe0844244bf56e677
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-23 12:52:49 +0300

    feat: placeOrder fn in orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,34 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
 // placing orders using COD Meethod
 
-const PlaceOrder = async (req, res) => {};
+const PlaceOrder = async (req, res) => {
+    try {
+        const { userId, items, amount, adderss } = req.body;
+        
+        const orderData = {
+            userId,
+            items,
+            adderss,
+            amount,
+            paymentMethod: 'COD',
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId, { cartData: {} })
+        
+        res.json({success:true, message:"Order Places"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+};
 // placing orders using Stripe Meethod
 
 const PlaceOrderStripe = async (req, res) => {};
