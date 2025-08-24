61a202feb0b0861c763e4b976e522467bec3eb6e
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-24 13:35:47 +0300

    feat: add allOrders controller to fetch and return all orders for admin panel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,7 +39,13 @@ const PlaceOrderPaypal = async (req, res) => {};
 
 // All Orders data for Admin panel
 const allOrders = async (req, res) => {
-    
+    try {
+        const orders = await orderModel.find({})
+        res.json({success:true, orders})
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 };
 
 // All Orders data for Frontend
--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -3,48 +3,49 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import Loader from "../components/Loader";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Orders = () => {
-  
-        const [orderData, setOrderData] = useState([]);
+    const [orderData, setOrderData] = useState([]);
 
-  const { backendUrl, token, currency } = useContext(ShopContext);
+    const { backendUrl, token, currency } = useContext(ShopContext);
     // // Show loader if products are not loaded yet
     // if (!orderData || orderData.length === 0) {
     //     return <Loader text="Loading orders..." />;
     // }
 
-
     const loadOrderData = async () => {
         try {
             if (!token) {
-                return null
+                return null;
             }
-            const response = await axios.post(backendUrl + '/api/order/userorders', {}, { headers: { token } })
+            const response = await axios.post(
+                backendUrl + "/api/order/userorders",
+                {},
+                { headers: { token } }
+            );
             if (response.data.success) {
-                let allOrdersItem = []
+                let allOrdersItem = [];
                 response.data.orders.map((order) => {
                     order.items.map((item) => {
-                        item['status'] = order.status
-                        item['payment'] = order.payment
-                        item['paymentMethod'] = order.paymentMethod
-                        item['date'] = order.date
-                        allOrdersItem.push(item)
-
-                    })
-                })
-                setOrderData(allOrdersItem.reverse())
+                        item["status"] = order.status;
+                        item["payment"] = order.payment;
+                        item["paymentMethod"] = order.paymentMethod;
+                        item["date"] = order.date;
+                        allOrdersItem.push(item);
+                    });
+                });
+                setOrderData(allOrdersItem.reverse());
             }
-
         } catch (error) {
-            console.log(error)
-
+            console.log(error);
+            toast.error(error.message);
         }
-    }
+    };
 
     useEffect(() => {
-        loadOrderData()
-    },[token])
+        loadOrderData();
+    }, [token]);
 
     return (
         <div className="border-t pt-16">
@@ -101,7 +102,10 @@ const Orders = () => {
                                     {item.status}
                                 </p>
                             </div>
-                            <button onClick={loadOrderData} className="border px-4 py-2 text-sm font-medium rounded-sm">
+                            <button
+                                onClick={loadOrderData}
+                                className="border px-4 py-2 text-sm font-medium rounded-sm"
+                            >
                                 Track Order
                             </button>
                         </div>
