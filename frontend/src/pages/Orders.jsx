d08b2a0c7785c4ea6fa149800bc3546c7af01166
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-24 15:16:39 +0300

    feat(admin): create initial Orders page with backend integration setup

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -9,10 +9,7 @@ const Orders = () => {
     const [orderData, setOrderData] = useState([]);
 
     const { backendUrl, token, currency } = useContext(ShopContext);
-    // // Show loader if products are not loaded yet
-    // if (!orderData || orderData.length === 0) {
-    //     return <Loader text="Loading orders..." />;
-    // }
+    
 
     const loadOrderData = async () => {
         try {
@@ -52,66 +49,70 @@ const Orders = () => {
             <div className="text-2xl">
                 <Title text1={"MY"} text2={"ORDERS"} />
             </div>
-
-            <div>
-                {orderData.map((item, index) => (
-                    <div
-                        key={index}
-                        className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row items-center md:justify-between gap-4"
-                    >
-                        <div className="flex items-start gap-6 text-sm">
-                            <img
-                                className="w-16 sm:w-20"
-                                src={item.images[0]}
-                                alt=""
-                            />
-                            <div>
-                                <p className="sm:text-base font-medium">
-                                    {item.name}
-                                </p>
-                                <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
-                                    <p className="text-lg">
-                                        {currency}
-                                        {item.price}
+            {/* Show loader if products are not loaded yet */}
+            {!orderData || orderData.length === 0 ? (
+                <Loader text="Loading orders..." />
+            ) : (
+                <div>
+                    {orderData.map((item, index) => (
+                        <div
+                            key={index}
+                            className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row items-center md:justify-between gap-4"
+                        >
+                            <div className="flex items-start gap-6 text-sm">
+                                <img
+                                    className="w-16 sm:w-20"
+                                    src={item.images[0]}
+                                    alt=""
+                                />
+                                <div>
+                                    <p className="sm:text-base font-medium">
+                                        {item.name}
                                     </p>
-                                    <p>Quantity: {item.quantity}</p>
-                                    <p>size: {item.size}</p>
+                                    <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
+                                        <p className="text-lg">
+                                            {currency}
+                                            {item.price}
+                                        </p>
+                                        <p>Quantity: {item.quantity}</p>
+                                        <p>size: {item.size}</p>
+                                    </div>
+                                    <p className="mt-1">
+                                        Date:
+                                        <span className="text-gray-400">
+                                            {new Date(item.date).toDateString()}
+                                        </span>
+                                    </p>
+                                    <p className="mt-1">
+                                        Payment:
+                                        <span className="text-gray-400">
+                                            {item.paymentMethod}
+                                        </span>
+                                    </p>
                                 </div>
-                                <p className="mt-1">
-                                    Date:
-                                    <span className="text-gray-400">
-                                        {new Date(item.date).toDateString()}
-                                    </span>
-                                </p>
-                                <p className="mt-1">
-                                    Payment:
-                                    <span className="text-gray-400">
-                                        {item.paymentMethod}
-                                    </span>
-                                </p>
                             </div>
-                        </div>
 
-                        <div className="md:w-1/2 flex justify-between">
-                            <div
-                                className="
+                            <div className="md:w-1/2 flex justify-between">
+                                <div
+                                    className="
                 flex items-center gap-2"
-                            >
-                                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
-                                <p className="text-sm md:text-base">
-                                    {item.status}
-                                </p>
+                                >
+                                    <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                                    <p className="text-sm md:text-base">
+                                        {item.status}
+                                    </p>
+                                </div>
+                                <button
+                                    onClick={loadOrderData}
+                                    className="border px-4 py-2 text-sm font-medium rounded-sm"
+                                >
+                                    Track Order
+                                </button>
                             </div>
-                            <button
-                                onClick={loadOrderData}
-                                className="border px-4 py-2 text-sm font-medium rounded-sm"
-                            >
-                                Track Order
-                            </button>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
